f784375e025480c0d24a1d1b9924cf0bddb8bd7a
lehieu30794 <lehieu30794@gmail.com>
2018-07-10 13:18:35 -0600

    Add two toppings to UI

--- app/src/main/java/com/example/android/justjavaaw/MainActivity.java
+++ app/src/main/java/com/example/android/justjavaaw/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.justjavaaw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.TextView;
 
