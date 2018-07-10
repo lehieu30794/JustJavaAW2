f23f905b80fbd4aa56ed449e2f012f107efaa019
lehieu30794 <lehieu30794@gmail.com>
2018-07-10 09:57:34 -0600

    Add some Java codes

--- app/src/main/java/com/example/android/justjavaaw/MainActivity.java
+++ app/src/main/java/com/example/android/justjavaaw/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.android.justjavaaw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int quantity = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void decrement(View view) {
+        quantity = quantity - 1;
+    }
+
+    public void displayQuantity(View view) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + quantity);
+    }
+
+    public void displaPrice(View view) {
+        TextView orderSummaryTextView = findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText("");
     }
 }
